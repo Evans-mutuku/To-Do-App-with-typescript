e65f15a3997110a21d63d6328b67b784539d5e0c
evans <evans@pop-os.localdomain>
2022-06-04 15:45:48 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,67 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from 'uuid'
+console.log("Hello world")
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// create local storage
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  // validate the input and if empty return null
+  if(input?.value == "" || input?.value == null) return
+
+  // create a new task with the following properties
+  const newTask: Task ={
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  tasks.push(newTask)
+  // call the function that add an item to the list
+  addListItem(newTask)
+  // clear input field after submit
+  input.value = ""
+})
+
+// in order to pass the types and reduce file size for clean code we define types independendly and pass them as parameters in the function bellow
+
+function addListItem  (task: Task){
+  const item = document.createElement("li")
+  const label = document.createElement('label')
+  const checkbox = document.createElement('input')
+  checkbox.addEventListener("change", () => {
+    task.completed =checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+
+  // compine all the field/ elements into one using the array append method
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+
+}
+
+const saveTasks = () => {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks():Task[] {
+  const taskJson = localStorage.getItem('TASKS')
+  if(taskJson == null) return []
+  return JSON.parse(taskJson)
+}
